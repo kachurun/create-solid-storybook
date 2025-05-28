92dab9c9893d64946da46e55cfaf3178500df687
Maksim Kachurin <kachurun@gmail.com>
2025-05-27 23:26:40 -0700

    Template from official storybook

--- packages/create-solid-storybook/template/.storybook/main.ts
+++ packages/create-solid-storybook/template/.storybook/main.ts
@@ -5,12 +5,16 @@ import type { StorybookConfig } from '@kachurun/storybook-solid-vite';
 export default <StorybookConfig>{
     framework: '@kachurun/storybook-solid-vite',
     addons: [
+        '@storybook/addon-onboarding',
         '@storybook/addon-docs',
-        '@storybook/addon-links',
         '@storybook/addon-a11y',
         '@storybook/addon-vitest',
+        '@storybook/addon-links',
     ],
-    stories: ['../stories/**/*.mdx', '../stories/**/*.stories.tsx'],
+    stories: [
+        '../stories/**/*.mdx',
+        '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+    ],
     async viteFinal(config) {
         return mergeConfig(config, {
             define: {
--- packages/create-solid-storybook/template/.storybook/preview.ts
+++ packages/create-solid-storybook/template/.storybook/preview.ts
@@ -1,25 +1,21 @@
-import addonA11y from '@storybook/addon-a11y';
-import addonDocs from '@storybook/addon-docs';
-import addonVitest from '@storybook/addon-vitest';
+import type { Preview } from '@kachurun/storybook-solid';
 
 export const tags = ['autodocs'];
 
-export const parameters = {
-    // automatically create action args for all props that start with "on"
-    actions: { argTypesRegex: '^on.*' },
-    controls: {
-        matchers: {
-            color: /(background|color)$/i,
-            date: /Date$/,
+const preview: Preview = {
+    parameters: {
+        // automatically create action args for all props that start with "on"
+        actions: { argTypesRegex: '^on.*' },
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/,
+            },
         },
-    },
-    docs: {
-        codePanel: true,
+        docs: {
+            codePanel: true,
+        },
     },
 };
 
-export const addons = [
-    addonA11y(),
-    addonDocs(),
-    addonVitest(),
-];
+export default preview;
